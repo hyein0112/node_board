215bde39efd1e7b902b4e58a3f74964f106d767d
hyein0112 <bhi12134@gmail.com>
2023-07-25 06:13:07 +0900

    member 테이블 profile_picture컬럼 추가

--- routes/auth.js
+++ routes/auth.js
@@ -17,7 +17,8 @@ router.post("/signup", (req, res) => {
 
   bcrypt.hash(params[1], 10, (_, hash) => {
     params[1] = hash;
-    const sql = "INSERT INTO member VALUES (?, ?, ?, ?)";
+    const sql =
+      "INSERT INTO member (id, pw, name, phone_number) VALUES (?, ?, ?, ?)";
     conn.query(sql, params, (err) => {
       if (err) throw err;
       else {
