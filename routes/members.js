48e2fee62d94e3227e83a46ee9518b53f3f295a0
hyein0112 <bhi12134@gmail.com>
2023-07-25 03:18:13 +0900

    update

--- routes/members.js
+++ routes/members.js
@@ -7,7 +7,7 @@ let conn = db_config.init();
 
 // 멤버 리스트 조회
 router.get("/list", (_, res) => {
-  var sql = "SELECT id, name, phone_number, address FROM member";
+  var sql = "SELECT id, name, phone_number FROM member";
   conn.query(sql, (err, data) => {
     if (err) throw err;
     else res.json({ data: data, message: "SUCCESS" });
@@ -20,7 +20,7 @@ router
   //멤버 조회
   .get((req, res) => {
     const { id } = req.params;
-    let sql = `SELECT id, name, phone_number, address FROM member WHERE id LIKE '${id}'`;
+    let sql = `SELECT id, name, phone_number FROM member WHERE id LIKE '${id}'`;
     conn.query(sql, (err, member) => {
       if (err) throw err;
       else if (member.length === 0) {
